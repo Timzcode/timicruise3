fe07e550874a1797d6392fe2cfe7c68ff06ebc93
Olivier Le Thanh Duong <olivier@lethanh.be>
2014-05-27 15:53:57 +0200

    Release 0.2
    This release doesn't add any functionnality, only cosmetic and packaging change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='tim',
-      version='0.1',
+      version='0.2',
       description='TIM: Timed Iteration Monitor',
       author='Olivier Le Thanh Duong',
       long_description=open("README.md", "r").read(),

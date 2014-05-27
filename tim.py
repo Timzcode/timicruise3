984cb51199c33d43cbcce7d53882fbc9d58c5c72
Olivier Le Thanh Duong <olivier@lethanh.be>
2014-05-27 15:27:56 +0200

    add licensing information

--- tim.py
+++ tim.py
@@ -1,9 +1,8 @@
 """
-start
-increment
-stop
-per pulse, last_pulse
-when do we print?
+ Copyright (c) 2014 Olivier Le Thanh Duong <olivier@lethanh.be>
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
 """
 from datetime import datetime
 

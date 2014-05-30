15e8418a8f1b529c81698f3c82077f39681bb6af
Olivier Le Thanh Duong <olivier@lethanh.be>
2014-05-30 23:50:33 +0200

    fix installation via pip

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 # -*- coding:Utf-8 -*-
-
 from setuptools import setup
+
+try:
+    long_description=open("README.md", "r").read(),
+except IOError:
+    long_description = ""
 
 setup(name='tim',
       version='0.4',
       description='TIM: Timed Iteration Monitor',
       author='Olivier Le Thanh Duong',
-      long_description=open("README.md", "r").read(),
+      long_description=long_description,
       author_email='olivier@lethanh.be',
       url='https://github.com/olethanh/tim',
       install_requires=[],
